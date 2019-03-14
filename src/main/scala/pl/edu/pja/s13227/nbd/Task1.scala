4384cd70ca9272e9d3637677b92583bc3960a3c3
Andrzej Goetzendorf-Grabowski <andrzejgrabowski90@gmail.com>
2019-03-14 22:58:40 +0100

    Implement subtasks 1a - 1f

--- src/main/scala/pl/edu/pja/s13227/nbd/Task1.scala
+++ src/main/scala/pl/edu/pja/s13227/nbd/Task1.scala
@@ -1,6 +1,6 @@
 package pl.edu.pja.s13227.nbd
 
-object Task1 extends App {
+object Task1 {
 
   val weekDays: List[String] = List(
     "Poniedzialek",
@@ -12,21 +12,68 @@ object Task1 extends App {
     "Niedziela"
   )
 
-  println("----------")
-  println("Petla for:")
-  println("----------")
+  def main(args: Array[String]): Unit = {
+    println("--------------------------------------")
+    println("Petla for:")
+    println("--------------------------------------")
 
-  for ( day <- weekDays) {
-    println(day)
-  }
-
-  println("--------------------------------------")
-  println("Petla for, dni zaczynajace sie na 'P':")
-  println("--------------------------------------")
-
-  for ( day <- weekDays) {
-    if (day.startsWith("P")) {
+    for ( day <- weekDays) {
       println(day)
     }
+
+    println("--------------------------------------")
+    println("Petla for, dni zaczynajace sie na 'P':")
+    println("--------------------------------------")
+
+    for ( day <- weekDays) {
+      if (day.startsWith("P")) {
+        println(day)
+      }
+    }
+
+    println("--------------------------------------")
+    println("Metoda forEach:")
+    println("--------------------------------------")
+
+    weekDays.foreach( { println } )
+
+    println("--------------------------------------")
+    println("Petla while:")
+    println("--------------------------------------")
+
+    var i = 0
+    while (i < weekDays.length) {
+      println(weekDays(i))
+      i = i + 1
+    }
+
+    println("--------------------------------------")
+    println("Funkcja rekurencyjna:")
+    println("--------------------------------------")
+
+    recursivePrint(0, weekDays)
+
+    println("--------------------------------------")
+    println("Odwrocona funkcja rekurencyjna:")
+    println("--------------------------------------")
+
+    reverseRecursivePrint(6, weekDays)
+
+  }
+
+  def recursivePrint(i : Int, list: List[String]): Unit = {
+    if (i < 0 || i >= list.length) {
+      return
+    }
+    println(list(i))
+    recursivePrint(i + 1, list)
+  }
+
+  def reverseRecursivePrint(i : Int, list: List[String]): Unit = {
+    if (i < 0 || i >= list.length) {
+      return
+    }
+    println(list(i))
+    reverseRecursivePrint(i - 1, list)
   }
 }
